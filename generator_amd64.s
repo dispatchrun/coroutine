8b65fdb3b910c4f2ea4f52dcfc54a4853d07f11b
Achille <achille.roussel@gmail.com>
2023-09-05 14:17:11 -0700

    Update generator_amd64.s
    Co-authored-by: Thomas Pelletier <github@pelletier.codes>

--- generator_amd64.s
+++ generator_amd64.s
@@ -4,7 +4,7 @@
 #include "go_tls.h"
 #include "testflag.h"
 
-// GOARCH=arm64 exposes the get_tls and g macros to access thread local storage
+// GOARCH=amd64 exposes the get_tls and g macros to access thread local storage
 // and the g pointer in it.
 //
 // See: https://go.dev/doc/asm (64-bit Intel 386)
