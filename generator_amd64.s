de0d9bca989d89e28fdfe7954f07c34b9db13f9b
Chris O'Hara <cohara87@gmail.com>
2023-09-06 08:26:40 +1000

    Fix amd64 build
    The go_tls.h header isn't accessible here, but the macros it
    defines are simple enough to be inlined.

--- generator_amd64.s
+++ generator_amd64.s
@@ -1,15 +1,18 @@
 //go:build !durable
 
 #include "go_asm.h"
-#include "go_tls.h"
-#include "testflag.h"
+#include "textflag.h"
 
 // GOARCH=amd64 exposes the get_tls and g macros to access thread local storage
-// and the g pointer in it.
+// and the g pointer in it. The routine here inlines the following macros
+// defined in $GOROOT/src/runtime/go_tls.h:
+//
+//  #define get_tls(r) MOVQ TLS, r
+//  #define g(r)       0(r)(TLS*1)
 //
 // See: https://go.dev/doc/asm (64-bit Intel 386)
 TEXT ·getg(SB), NOSPLIT, $0-8
-    get_tls(CX)
-    MOVQ g(CX), AX
+    MOVQ TLS, CX
+    MOVQ 0(CX)(TLS*1), AX
     MOVQ AX, ret+0(FP)
     RET
