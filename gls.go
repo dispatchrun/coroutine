361b33ac2cad24795a611c61f6cfe7074dfa7a76
Achille Roussel <achille.roussel@gmail.com>
2023-09-05 17:19:53 -0700

    document an alternative implementation for GLS
    Signed-off-by: Achille Roussel <achille.roussel@gmail.com>

--- gls.go
+++ gls.go
@@ -11,6 +11,16 @@ import "sync"
 //   - create a sharded map with 64 buckets, each bucket contains a map
 //   - use a sync.Mutex in each bucket for synchronization; cheaper than RWMutex
 //   - mask the value of g to determine in which bucket its GLS is stored
+//
+// An alternative to using a global map could be to analyze the memory layout of
+// the runtime.g type and determine if there is spare room after the struct to
+// store the Context pointer: the Go memory allocate uses size classes to park
+// objects in buckets, there is often spare space after large values like the
+// runtime.g type since they will be assigned to the size class greater or equal
+// to their type size. We only need 4 or 8 bytes of spare space on 32 or 64 bit
+// architectures. This approach would remove all potential contention accessing
+// and synchronizing on global state, and would also turn the map lookups into
+// simple memory loads.
 var (
 	gmutex sync.RWMutex
 	gstate map[uintptr]any
