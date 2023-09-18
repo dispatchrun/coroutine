f5d5c7d6ece4b5825dcbf1cb8d82759040567b1e
Julien Fabre <ju.pryz@gmail.com>
2023-09-18 13:57:09 -0600

    simple

--- serde/types.go
+++ serde/types.go
@@ -10,31 +10,16 @@ func init() {
 }
 
 func serializeTime(s *Serializer, x *time.Time) error {
-	// see: https://github.com/golang/go/blob/38b2c06e144c6ea7087c575c76c66e41265ae0b7/src/time/time.go#L1240
-	off := 1
-	_, offset := x.Zone()
-	if offset%60 != 0 {
-		off = 0
-	}
-
-	b := make([]byte, 16)
-
 	data, err := x.MarshalBinary()
 	if err != nil {
 		return fmt.Errorf("failed to marshal time.Time: %w", err)
 	}
-	copy(b[off:], data)
-
-	Serialize(s, b)
+	Serialize(s, data)
 	return nil
 }
 
 func deserializeTime(d *Deserializer, x *time.Time) error {
-	buf := make([]byte, 16)
-	DeserializeTo(d, &buf)
-	off := 1
-	if buf[0] > 0 {
-		off = 0
-	}
-	return x.UnmarshalBinary(buf[off:])
+	var b []byte
+	DeserializeTo(d, &b)
+	return x.UnmarshalBinary(b)
 }
