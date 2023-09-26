9aa3f3944701186f0fc5757ad80672e753322a43
Chris O'Hara <cohara87@gmail.com>
2023-09-26 16:50:49 -0700

    Improve readability

--- examples/scrape/main.go
+++ examples/scrape/main.go
@@ -11,32 +11,30 @@ import (
 )
 
 func main() {
-	s := NewScraper("https://en.wikipedia.org/wiki/Main_Page", 100)
+	scraper := NewScraper("https://en.wikipedia.org/wiki/Main_Page", 100)
 
-	c := coroutine.New[string, any](func() {
-		s.Start()
+	coro := coroutine.New[string, any](func() {
+		scraper.Start()
 	})
 
 	// Restore state.
-	ctx := c.Context()
 	state, err := os.ReadFile(".state")
 	if err != nil {
 		if !errors.Is(err, os.ErrNotExist) {
 			log.Fatal(err)
 		}
-	} else if _, err := ctx.Unmarshal(state); err != nil {
+	} else if _, err := coro.Context().Unmarshal(state); err != nil {
 		log.Fatal(err)
 	}
 
-	for c.Next() {
-		url := c.Recv()
+	for coro.Next() {
+		url := coro.Recv()
 		log.Println("scraped", url)
 
-		b, err := c.Context().Marshal()
-		if err != nil {
+		// Persist state.
+		if b, err := coro.Context().Marshal(); err != nil {
 			log.Fatal(err)
-		}
-		if err := os.WriteFile(".state", b, 0644); err != nil {
+		} else if err := os.WriteFile(".state", b, 0644); err != nil {
 			log.Fatal(err)
 		}
 	}
