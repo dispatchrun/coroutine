90b3724b548e52fc80b006d946624427c5772e98
Achille Roussel <achille.roussel@gmail.com>
2023-10-02 04:32:25 +0000

    test with stack growth
    Signed-off-by: Achille Roussel <achille.roussel@gmail.com>

--- coroutine_test.go
+++ coroutine_test.go
@@ -6,7 +6,7 @@ import (
 	"unsafe"
 )
 
-func TestGLS(t *testing.T) {
+func TestLocalStorage(t *testing.T) {
 	ch := make(chan any)
 	key := uintptr(0)
 	val := any(42)
@@ -20,7 +20,31 @@ func TestGLS(t *testing.T) {
 	}
 }
 
-func BenchmarkGLS(b *testing.B) {
+//go:noinline
+func weirdLoop(n int, f func()) int {
+	if n == 0 {
+		f()
+		return 0
+	} else {
+		return weirdLoop(n-1, f) + 1 // just in case Go ever implements tail recursion
+	}
+}
+
+func TestLocalStorageGrowStack(t *testing.T) {
+	ch := make(chan any)
+	key := uintptr(0)
+	val := any(42)
+
+	go with(&key, val, func() {
+		weirdLoop(100e3, func() { ch <- load(key) })
+	})
+
+	if v := <-ch; !reflect.DeepEqual(v, val) {
+		t.Errorf("wrong value for key=%v: %v", key, *(*[2]unsafe.Pointer)(unsafe.Pointer(&v)))
+	}
+}
+
+func BenchmarkLocalStorage(b *testing.B) {
 	key := uintptr(0)
 	val := any(42)
 	with(&key, val, func() {
