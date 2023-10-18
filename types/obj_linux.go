2f2ab32b8dff5898b9ef6f8b9869e25ee561500c
Chris O'Hara <cohara87@gmail.com>
2023-10-19 09:22:28 +1000

    Parse build ID from ELF binaries

--- types/obj_linux.go
+++ types/obj_linux.go
@@ -1,18 +1,20 @@
 package types
 
 import (
+	"bytes"
 	"debug/elf"
 	"os"
 )
 
 func init() {
-	obj, err := elf.Open(os.Args[0])
+	f, err := elf.Open(os.Args[0])
 	if err != nil {
 		panic("cannot read elf binary: " + err.Error())
 	}
-	defer obj.Close()
+	defer f.Close()
 
-	initELFFunctionTables(obj)
+	initELFFunctionTables(f)
+	initELFBuildID(f)
 }
 
 func initELFFunctionTables(f *elf.File) {
@@ -28,3 +30,27 @@ func initELFFunctionTables(f *elf.File) {
 	}
 	initFunctionTables(pclntabData, symtabData)
 }
+
+func initELFBuildID(f *elf.File) {
+	noteSection := f.Section(".note.go.buildid")
+	note, err := readSection(noteSection, noteSection.Size)
+	if err != nil {
+		panic("cannot read build ID: " + err.Error())
+	}
+
+	// See https://github.com/golang/go/blob/3803c858/src/cmd/internal/buildid/note.go#L135C3-L135C3
+	nameSize := f.ByteOrder.Uint32(note)
+	valSize := f.ByteOrder.Uint32(note[4:])
+	tag := f.ByteOrder.Uint32(note[8:])
+	nname := note[12:16]
+	if nameSize == 4 && 16+valSize <= uint32(len(note)) && tag == buildIDTag && bytes.Equal(nname, buildIDNote) {
+		buildid = string(note[16 : 16+valSize])
+	} else {
+		panic("build ID not found")
+	}
+}
+
+var (
+	buildIDNote = []byte("Go\x00\x00")
+	buildIDTag  = uint32(4)
+)
