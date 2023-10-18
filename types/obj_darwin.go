a1a3fcbbb93aa1bdbd42a0683b2219313524a9e8
Chris O'Hara <cohara87@gmail.com>
2023-10-19 08:17:16 +1000

    Move helpers

--- types/obj_darwin.go
+++ types/obj_darwin.go
@@ -2,6 +2,7 @@ package types
 
 import (
 	"debug/macho"
+	"fmt"
 	"os"
 )
 
@@ -15,4 +16,19 @@ func init() {
 	if err := initMachOFunctionTables(f); err != nil {
 		panic(err)
 	}
+}
+
+func initMachOFunctionTables(f *macho.File) error {
+	pclntab := f.Section("__gopclntab")
+	pclntabData, err := readAll(pclntab, pclntab.Size)
+	if err != nil {
+		return fmt.Errorf("cannot read pclntab: %w", err)
+	}
+	symtab := f.Section("__gosymtab")
+	symtabData, err := readAll(symtab, symtab.Size)
+	if err != nil {
+		return fmt.Errorf("cannot read symtab: %w", err)
+	}
+	initFunctionTables(pclntabData, symtabData)
+	return nil
 }
--- types/obj_linux.go
+++ types/obj_linux.go
@@ -11,3 +11,18 @@ func init() {
 		panic(err)
 	}
 }
+
+func initELFFunctionTables(f *elf.File) error {
+	pclntab := f.Section(".gopclntab")
+	pclntabData, err := readAll(pclntab, pclntab.Size)
+	if err != nil {
+		return fmt.Errorf("cannot read pclntab: %w", err)
+	}
+	symtab := f.Section(".gosymtab")
+	symtabData, err := readAll(symtab, symtab.Size)
+	if err != nil {
+		return fmt.Errorf("cannot read symtab: %w", err)
+	}
+	initFunctionTables(pclntabData, symtabData)
+	return nil
+}
